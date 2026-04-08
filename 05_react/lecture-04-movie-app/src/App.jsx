90e1e23b2044181610a94f24252d8627ef5983c6
molang404 <art104233@naver.com>
2026-04-08 17:58:35 +0900

    MovieCard 작업

--- 05_react/lecture-04-movie-app/src/App.jsx
+++ 05_react/lecture-04-movie-app/src/App.jsx
@@ -1,5 +1,40 @@
-function App() {
+import { useEffect, useState } from "react";
+import MovieCard from "./components/MovieCard.jsx";
 
+function App() {
+    const [loading, setLoading] = useState(true); // 불러올 때의 loading 상태를 관리할 state
+    const [movies, setMovies] = useState([]); // 불러온 무비 목록을 저장할 state
+
+    useEffect(() => {
+        fetch("https://imdb.iamidiotareyoutoo.com/search?q=2026&lsn=1&v=1")
+            .then(res => res.json())
+            .then(json => {
+                setMovies(json.description);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.log("데이터 수신 오류", error);
+            });
+    }, []);
+
+    return (
+        <div>
+            {loading ? (
+                <h1>Loading...</h1>
+            ) : (
+                <div>
+                    {movies.map(value => (
+                        <MovieCard
+                            key={value["#INDB_ID"]}
+                            poster={value["#IMG_POSTER"]}
+                            title={value["#TITLE"]}
+                            actors={value["#ACTORS"]}
+                        />
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
